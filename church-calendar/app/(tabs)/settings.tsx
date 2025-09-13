43ef8c53dd7b3cb3cccafceb76847ba968059be3
Lazaro Altedill <lazaroah02@gmail.com>
2025-09-13 18:55:07 -0400

    feat: creating settings page basic structure

--- church-calendar/app/(tabs)/settings.tsx
+++ church-calendar/app/(tabs)/settings.tsx
@@ -1,22 +1,63 @@
-import { Pressable, StyleSheet, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useSession } from '@/contexts/authContext';
-import { Link } from 'expo-router';
-import { navigate } from 'expo-router/build/global-state/routing';
+import { Pressable, ScrollView, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useSession } from "@/contexts/authContext";
+import { Link } from "expo-router";
+import { navigate } from "expo-router/build/global-state/routing";
 
 export default function Settings() {
-  const {signOut} = useSession()
+  const { signOut } = useSession();
   return (
     <SafeAreaView>
-      <Link style = {{color:"red"}} href="/welcome">Sign in</Link>
-      <Pressable onPress={() => {
-        signOut()
-        navigate("/welcome")
-        }}><Text style={{color:"green"}}>Sign out</Text></Pressable>
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}>Opciones</Text>
+      </ScrollView>
+      <Pressable
+        style={styles.signOutButton}
+        onPress={() => {
+          signOut();
+          navigate("/welcome");
+        }}
+      >
+        <Text style={styles.signOutButtonText}>Cerrar Sesión</Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container:{
+    flexGrow:1,
+    height:"90%",
+    padding:10
+  },
+  title:{
+    width: 200,
+    textAlign: "center",
+    color: "#000",
+    fontFamily: "InterBold",
+    fontSize: 25,
+    fontWeight: "700",
+  },
+  signOutButton: {
+    alignSelf:"center",
+    width: 330,
+    height: 55,
+    backgroundColor: "#B5B5B5",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    marginVertical: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 4, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 4,
+  },
+  signOutButtonText: {
+    color: "#000",
+    fontSize: 16,
+    fontWeight: "500",
+    fontFamily: "InterMedium",
+  },
 });
