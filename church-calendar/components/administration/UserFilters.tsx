24cb8fc3e80357baa861209b7744dc6998027407
Lazaro Altedill <lazaroah02@gmail.com>
2025-11-18 14:18:04 -0500

    feat: adding action buttons to user filters bottomsheet

--- church-calendar/components/administration/UserFilters.tsx
+++ church-calendar/components/administration/UserFilters.tsx
@@ -3,14 +3,21 @@ import { AppTheme } from "@/theme";
 import { Ionicons } from "@expo/vector-icons";
 import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
 import { useMemo, useRef } from "react";
-import { Animated, Pressable, View, Text, TouchableOpacity } from "react-native";
+import {
+  Animated,
+  Pressable,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { CustomBottomSheetBackground } from "../calendar/events-bottom-sheet";
 import { CheckBox } from "../form/checkbox";
 import { ChurchGroupsPicker } from "../form/church-groups-picker";
+import { Button } from "../Button";
 
 export function UserFiltersBottomSheet() {
   const sheetRef = useRef<BottomSheet>(null);
-  const snapPoints = useMemo(() => ["95%"], []);
+  const snapPoints = useMemo(() => ["98%"], []);
   const styles = useThemeStyles(bottomSheetStyles);
 
   const openUserFiltersBottomSheet = () => {
@@ -39,17 +46,55 @@ export function UserFiltersBottomSheet() {
       <BottomSheetScrollView>
         <Text style={styles.title}>Filtros</Text>
 
-        <View style = {styles.content}>
-          <CheckBox label="Es Administrador" checked={false} onCheck={(checked) => null}/>
-          <View style={{height:10}}></View>
-          <CheckBox label="Tiene acceso a la aplicación" checked={false} onCheck={(checked) => null}/>
-          <View style={{height:15}}></View>
-          <ChurchGroupsPicker onChange={(selectedGroups) => null} placeholder="Filtrar por grupo"/>
+        <View style={styles.content}>
+          <CheckBox
+            label="Solo Administradores"
+            checked={false}
+            onCheck={(checked) => null}
+          />
+          <CheckBox
+            label="No Administradores"
+            checked={false}
+            onCheck={(checked) => null}
+          />
+          <CheckBox
+            label="Tiene acceso a la aplicación"
+            checked={false}
+            onCheck={(checked) => null}
+          />
+          <CheckBox
+            label="No tiene acceso a la aplicación"
+            checked={false}
+            onCheck={(checked) => null}
+          />
+          <ChurchGroupsPicker
+            onChange={(selectedGroups) => null}
+            placeholder="Filtrar por grupo"
+            containerStyle={{ marginTop: 10 }}
+          />
+        </View>
+
+        <View style={styles.buttonsContainer}>
+          <Button
+            text="Limpiar"
+            onPress={() => {
+              closeUserFiltersBottomSheet()
+            }}
+            style={{ width: "45%" }}
+          />
+          <Button
+            text="Aplicar"
+            onPress={() => {
+              closeUserFiltersBottomSheet()
+            }}
+            style={{ width: "50%" }}
+            variant="submit"
+          />
         </View>
       </BottomSheetScrollView>
     </BottomSheet>
   );
-  
+
   return {
     userFiltersBottomSheet,
     openUserFiltersBottomSheetButton,
@@ -65,7 +110,13 @@ const bottomSheetStyles = (theme: AppTheme) => ({
     fontSize: 25,
     fontWeight: 700,
   },
-  content:{
+  content: {
     padding: 15,
-  }
+    gap: 15,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 15,
+  },
 });
