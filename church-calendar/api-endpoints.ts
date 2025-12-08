f126c1d4235410b9c25e71362a7ea4c162f0d25d
Lazaro Altedill <lazaroah02@gmail.com>
2025-12-08 13:25:22 -0500

    feat: updating production server endpoint

--- church-calendar/api-endpoints.ts
+++ church-calendar/api-endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === "development" ? 'http://192.168.1.199:8000' : 'https://churchcalendarbackend.pythonanywhere.com';
+export const BASE_URL = process.env.NODE_ENV === "development" ? 'http://192.168.1.199:8000' : 'https://laresurreccioncalendar.pythonanywhere.com';
 
 export const EVENTS_URL = `${BASE_URL}/api/events`
 export const CHURCH_GROUPS_URL = `${BASE_URL}/api/church-groups/`
